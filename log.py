08eb01e4233bce7fcb3872b90746f58b840f39ee
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-10-24 01:25:24 +0200

    Updated, extended and refactored test suite

--- log.py
+++ log.py
@@ -3,20 +3,23 @@ import sys
 class Logger:
     '''A very simple logger which logs to screen based on verbosity.'''
     
-    def __init__(self, verbose=True) -> None: #, infostream=sys.stdout, criticalstream=sys.stderr) -> None:
+    def __init__(self, verbose=True, infostream=sys.stdout, criticalstream=sys.stderr) -> None:
         self.verbose = verbose
-        #self.infostream = infostream
-        #self.criticalstream = criticalstream
+        self.infostream = infostream
+        self.criticalstream = criticalstream
     
     def set_verbose(self, verbose: bool) -> None:
         self.verbose = verbose
     
+    def get_verbose(self) -> bool:
+        return self.verbose
+    
     def info_verbose(self, msg: str) -> None:
         if self.verbose:
-            print("[*] " + msg)#, file=self.infostream)
+            print("[*] " + msg, file=self.infostream)
 
     def info(self, msg: str) -> None:
-        print("[*] " + msg)#, file=self.infostream)
+        print("[*] " + msg, file=self.infostream)
     
     def critical(self, msg: str) -> None:
-        print("[X] " + msg, file=sys.stderr)#, file=self.criticalstream)
+        print("[X] " + msg, file=self.criticalstream)
---
+++ test_log.py
@@ -0,0 +1,55 @@
+import log
+
+import io
+import pytest
+import sys
+
+
+def test_logger_get_set_verbose():
+    logger = log.Logger(verbose=True)
+    assert logger.get_verbose() == True
+
+    logger.set_verbose(False)
+    assert logger.get_verbose() == False
+
+    logger.set_verbose(True)
+    assert logger.get_verbose() == True
+
+
+def test_logger_critical():
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=True, criticalstream=captured_output)
+    logger.critical("abcdefg")
+    assert captured_output.getvalue() == "[X] abcdefg\n"
+
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=False, criticalstream=captured_output)
+    logger.critical("abcdefg")
+    assert captured_output.getvalue() == "[X] abcdefg\n"
+
+
+def test_logger_info_verbose():
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=True, infostream=captured_output)
+    logger.info_verbose("abcdefg")
+    assert captured_output.getvalue() == "[*] abcdefg\n"
+
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=False, infostream=captured_output)
+    logger.info_verbose("abcdefg")
+    assert captured_output.getvalue() == ""
+
+
+def test_logger_info():
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=True, infostream=captured_output)
+    logger.info("abcdefg")
+    assert captured_output.getvalue() == "[*] abcdefg\n"
+
+    captured_output = io.StringIO()
+    logger = log.Logger(verbose=False, infostream=captured_output)
+    logger.info("abcdefg")
+    assert captured_output.getvalue() == "[*] abcdefg\n"
+
+
+
