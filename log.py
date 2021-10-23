5eacbaaa9fb6607378ab2b65457899e3f31b17cb
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-10-24 00:09:52 +0200

    Add files via upload

--- log.py
+++ log.py
@@ -3,18 +3,20 @@ import sys
 class Logger:
     '''A very simple logger which logs to screen based on verbosity.'''
     
-    def __init__(self, verbose=True) -> None:
+    def __init__(self, verbose=True) -> None: #, infostream=sys.stdout, criticalstream=sys.stderr) -> None:
         self.verbose = verbose
+        #self.infostream = infostream
+        #self.criticalstream = criticalstream
     
     def set_verbose(self, verbose: bool) -> None:
         self.verbose = verbose
     
     def info_verbose(self, msg: str) -> None:
         if self.verbose:
-            print("[*] " + msg)
+            print("[*] " + msg)#, file=self.infostream)
 
     def info(self, msg: str) -> None:
-        print("[*] " + msg)
+        print("[*] " + msg)#, file=self.infostream)
     
     def critical(self, msg: str) -> None:
-        print("[X] " + msg, file=sys.stderr)
+        print("[X] " + msg, file=sys.stderr)#, file=self.criticalstream)
