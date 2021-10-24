427f2d2a9d09cca7b48902965a10913306e451e5
Ma-Fi-94 <33027321+Ma-Fi-94@users.noreply.github.com>
2021-10-24 16:30:00 +0200

    Add files via upload

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,7 @@
 yapf -i tinySSG.py
-yapf -i test_all.py
+yapf -i test_log.py
+yapf -i test_tinySSG.py
 mypy tinySSG.py
+mypy test_log.py
+mypy test_tinySSG.py
 py.test --cov-report term-missing --cov=. -v
